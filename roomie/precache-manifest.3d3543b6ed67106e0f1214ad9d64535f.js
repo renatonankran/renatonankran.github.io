6afa28294899c535b5f18de51cef7b38ea8907d6
renatonankran <renato.nankran.m@gmail.com>
2019-01-27 16:26:38 -0200

    Updating

---
+++ roomie/precache-manifest.3d3543b6ed67106e0f1214ad9d64535f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1995a7b1af9a16e9ed8883c40f627650",
+    "url": "/roomie/static/media/pexels-photo-555790.1995a7b1.png"
+  },
+  {
+    "revision": "28603fced93eeb3c63ab457c25fe1e28",
+    "url": "/roomie/static/media/Ativo-4.28603fce.svg"
+  },
+  {
+    "revision": "4b8f5db1e83bc9b471cc",
+    "url": "/roomie/static/js/runtime~main.4b8f5db1.js"
+  },
+  {
+    "revision": "77932726558c6125deef",
+    "url": "/roomie/static/js/main.77932726.chunk.js"
+  },
+  {
+    "revision": "7edc2f7a764900dc0712",
+    "url": "/roomie/static/js/1.7edc2f7a.chunk.js"
+  },
+  {
+    "revision": "77932726558c6125deef",
+    "url": "/roomie/static/css/main.93ae53f7.chunk.css"
+  },
+  {
+    "revision": "e849f9d813773c1f88e4e7186b0c4c84",
+    "url": "/roomie/index.html"
+  }
+];
